2361eec3d9e7d7f1a3120cbaf367717d05262037
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2022-03-03 01:44:55 +0300

    chart eklendi :)

--- panel/src/pages/chart/list.tsx
+++ panel/src/pages/chart/list.tsx
@@ -9,39 +9,65 @@ import {
   EditButton,
   DeleteButton,
 } from "@pankod/refine-antd";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const toPercent = (decimal: number, fixed = 0) => `${decimal.toFixed(0)}%`;
 export const AnalizChart = () => {
   const { tableProps } = useTable<IAnaliz>({
     initialPageSize: 1000,
   });
-  console.log("tableProps", tableProps);
+  const data = tableProps?.dataSource || [];
+  // console.log("data", data);
   return (
     <List>
-      {/* <Table {...tableProps} rowKey="id">
-        <Table.Column dataIndex="id" title="ID" />
-        <Table.Column dataIndex="su" title="Su" />
-        <Table.Column dataIndex="potasyum" title="Potasyum" />
-        <Table.Column dataIndex="fosfat" title="Fosfat" />
-        <Table.Column dataIndex="azot" title="Azot" />
-        <Table.Column
-          dataIndex="analiz_date"
-          title="Analiz Tarihi"
-          render={(value) => <DateField format="LLL" value={value} />}
-        />
-        <Table.Column
-          dataIndex="createdAt"
-          title="Kayıt Tarihi"
-          render={(value) => <DateField format="LLL" value={value} />}
-        />
-        <Table.Column<IAnaliz>
-          title="**"
-          render={(text, item) => (
-            <>
-              <ShowButton size="small" recordItemId={item.id} hideText />
-              <EditButton size="small" recordItemId={item.id} hideText />
-              <DeleteButton size="small" recordItemId={item.id} hideText />
-            </>
-          )}
-        />
+      <div
+        style={{
+          width: "100%",
+          minHeight: "400px",
+          height: "100%",
+        }}
+      >
+        <ResponsiveContainer width="100%" height={500}>
+        <LineChart
+          width={800}
+          height={400}
+          data={data as any}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="analiz_date" tickCount={6} format='YYYY'/>
+          <YAxis tickFormatter={toPercent} />
+          <Tooltip />
+          <Legend />
+          {/* <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          /> */}
+          <Line type="monotone" dataKey="su" stroke="black" />
+          <Line type="monotone" dataKey="azot" stroke="orange" />
+          <Line type="monotone" dataKey="potasyum" stroke="red" />
+          <Line type="monotone" dataKey="fosfat" stroke="yellowgreen" />
+        </LineChart>
+        </ResponsiveContainer>
+      </div>
+      {/* <Table {...tableProps}>
+
       </Table> */}
     </List>
   );
